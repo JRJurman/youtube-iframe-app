7bb90061d6221ddbe7a035ce8cff0fd657912e44
jrjurman <j.r.jurman@gmail.com>
2020-11-29 11:59:35 -0500

    add png and meta tags

--- src/addInlineManifest.js
+++ src/addInlineManifest.js
@@ -5,7 +5,8 @@ const html = registerHtml()
 export default ({videoId, appTitle, autoplay, controls}) => {
   const iframeParams = `?videoId=${videoId}&appTitle=${appTitle}&autoplay=${autoplay ? 1 : 0}&controls=${controls? 1 : 0}`
 
-  const svgIconHref = document.querySelector('link[rel="icon"]').href
+  const svgIconHref = document.querySelector('link[type="image/svg+xml"]').href
+  const pngIconHref = document.querySelector('link[type="image/png"]').href
   const manifest = JSON.stringify({
     name: appTitle,
     short_name: appTitle,
@@ -16,6 +17,10 @@ export default ({videoId, appTitle, autoplay, controls}) => {
         src: svgIconHref,
         sizes: "150x150"
       },
+      {
+        src: pngIconHref,
+        sizes: "512x512"
+      }
     ],
     display: "fullscreen",
     theme_color: "rgb(187, 119, 119)"
